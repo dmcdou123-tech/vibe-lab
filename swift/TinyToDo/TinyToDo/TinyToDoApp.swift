1ce4933cfd265799466bd75d7fa7137566653e20
David McDougal <dmcdou123@gmail.com>
2026-02-03 11:30:35 -0600

    add tinytodo xcode project

---
+++ swift/TinyToDo/TinyToDo/TinyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyToDoApp.swift
+//  TinyToDo
+//
+//  Created by David McDougal on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TinyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
